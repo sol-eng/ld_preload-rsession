3c7709b4330b405a3565cd3145d0352eeb6fc396
Michael Mayer <michael.mayer@rstudio.com>
2022-02-22 14:50:18 +0000

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ld_preload-rsession
+
+* change `upper` and `lower` in `shim_bind.c` for your local firewall settings 
+* compile `shim_bind.c` via `gcc -Wall -O2 -fpic -shared -ldl -o shim_bind.so shim_bind.c`
+* copy `shim_bind.so` to a shared location 
+* edit `rsession-profile` to make sure `LD_PRELOAD` points to the actual path of `shim_bind.so` 
+* add `rsession-profile` to your RStudio configuration files in `/etc/rstudio` 
+
+
+Note: If you would like to use another directory than `/etc/rstudio`, please consider the approach described in [the Admin Guide](https://docs.rstudio.com/ide/server-pro/server_management/core_administrative_tasks.html#alternate-configuration-file-location) 
---
+++ shim_bind.c
@@ -0,0 +1,40 @@
+/*
+ * capture calls to a routine and replace with your code
+ * http://unix.stackexchange.com/a/305336/119298
+ * gcc -Wall -O2 -fpic -shared -ldl -o shim_bind.so shim_bind.c
+ * LD_PRELOAD=/path/to/shim_bind.so ./test
+ */
+#define _GNU_SOURCE /* needed to get RTLD_NEXT defined in dlfcn.h */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dlfcn.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen){
+    static int (*real_bind)(int sockfd, const struct sockaddr *addr,
+                            socklen_t addrlen) = NULL;
+    int lower = 59000;
+    int upper = 59999; 
+
+    int port = (rand() % (upper - lower + 1)) + lower;
+
+    //int port = 7777;
+    struct sockaddr_in theaddr;
+
+    if (!real_bind) {
+        real_bind = dlsym(RTLD_NEXT, "bind");
+        char *error = dlerror();
+        if (error != NULL) {
+            fprintf(stderr, "%s\n", error);
+            exit(1);
+        }
+    }
+    fprintf(stderr, "binding: port %d\n", port);
+    memcpy(&theaddr, addr, sizeof(theaddr));
+    theaddr.sin_port = htons((unsigned short)port);
+    return real_bind(sockfd, (struct sockaddr*)&theaddr, addrlen);
+}
